db985cbe7ddb4b9b7d7571cd33cbc21b357a9a73
renanrambul <renanrambuls@gmail.com>
2025-04-27 14:50:47 -0300

    feat: update thumbnail

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -2,6 +2,33 @@ import { NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { defaultLocale } from "../../../i18n.config";
 import { ThemeProvider } from "@/providers/ThemeProvider";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Renan Rambul | Portfolio",
+  description: "Renan Rambul - Software Developer Portfolio",
+  icons: {
+    icon: [
+      { url: "/favicon.svg", type: "image/svg+xml" },
+      {
+        url: "/favicon/favicon-16x16.png.svg",
+        sizes: "16x16",
+        type: "image/svg+xml",
+      },
+      {
+        url: "/favicon/favicon-32x32.png.svg",
+        sizes: "32x32",
+        type: "image/svg+xml",
+      },
+      {
+        url: "/favicon/favicon-48x48.png.svg",
+        sizes: "48x48",
+        type: "image/svg+xml",
+      },
+    ],
+    apple: "/favicon/favicon.svg",
+  },
+};
 
 export function generateStaticParams() {
   return [{ locale: "en" }, { locale: "pt" }];
